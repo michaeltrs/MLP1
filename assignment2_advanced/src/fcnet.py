12ce5966d5a58bf9e7a465f40beaa4f1bfa10127
Michael Tarasiou <mtarasiou@gmail.com>
2018-02-27 20:10:03 +0000

    Add files via upload

--- assignment2_advanced/src/fcnet.py
+++ assignment2_advanced/src/fcnet.py
@@ -72,20 +72,17 @@ class FullyConnectedNet(object):
         # input -> first hidden layer
         self.params["W1"], self.params["b1"] = \
             random_init(input_dim, hidden_dims[0], weight_scale, dtype)
-        #print("shape of W1: ", self.params["W1"].shape)
-        #print("shape of b1: ", self.params["b1"].shape)
+
         # hidden layer i-1 -> hidden layer i
         for l in range(1, self.num_layers-1):
             self.params["W%d"%(l+1)],  self.params["b%d"%(l+1)] = \
                 random_init(hidden_dims[l-1], hidden_dims[l], weight_scale, dtype)
-            #print("shape of W%d: "%(l+1), self.params["W%d"%(l+1)].shape)
-            #print("shape of b%d: "%(l+1), self.params["b%d"%(l+1)].shape)
+
         # last hidden layer -> output
         l += 1
         self.params["W%d" % (l+1)], self.params["b%d" % (l+1)] = \
             random_init(hidden_dims[l-1], num_classes, weight_scale, dtype)
-        #print("shape of W%d: " % (l + 1), self.params["W%d"%(l+1)].shape)
-        #print("shape of b%d: " % (l + 1), self.params["b%d"%(l+1)].shape)
+
         #######################################################################
         #                            END OF YOUR CODE                         #
         #######################################################################
@@ -101,7 +98,20 @@ class FullyConnectedNet(object):
         # Cast all parameters to the correct datatype
         for k, v in self.params.items():
             self.params[k] = v.astype(dtype)
-        # print(self.dropout_params)
+
+    def apply_forward_dropout(self, x):
+        x_, mask = dropout_forward(x, p=self.dropout_params["p"],
+                                  train=self.dropout_params["train"],
+                                  seed=self.dropout_params["seed"])
+        return x_, mask
+
+    def apply_backward_dropout(self, dx, mask):
+        dx_ = dropout_backward(dout=dx,
+                              mask=mask,
+                              p=self.dropout_params["p"],
+                              train=self.dropout_params["train"])
+
+        return dx_
 
     def loss(self, X, y=None):
         """
@@ -135,56 +145,28 @@ class FullyConnectedNet(object):
         #######################################################################
 
         # input -> first hidden layer
-        if self.use_dropout:
-            input_next, mask = dropout_forward(X,  p=self.dropout_params["p"],
-                train=self.dropout_params["train"], seed=self.dropout_params["seed"])
-            dropout_cache[1] = mask
-            #print("shape of dropout to layer 1: ", input_next.shape)
-            if y is not None:
-                input_next = (1 - self.dropout_params["p"]) * activation_out
-        else:
-            input_next = X.copy()
-            #print("shape of input to layer 1: ", input_next.shape)
-        linear_cache[1] = linear_forward(input_next, self.params["W1"], self.params["b1"])
-        #print("shape of linear cache 1: ", linear_cache[1].shape)
-        activation_out = relu_forward(linear_cache[1])
-        relu_cache[1] = activation_out
-        #print("shape of relu cache 1: ", relu_cache[1].shape)
+        linear_cache[1] = linear_forward(X, self.params["W1"], self.params["b1"])
+        input_next = relu_forward(linear_cache[1])
+        relu_cache[1] = input_next.copy()
 
         # hidden layer i -> hidden layer i+1
         for l in range(2, self.num_layers):
             if self.use_dropout:
-                input_next, mask = dropout_forward(activation_out, p=self.dropout_params["p"],
-                    train=self.dropout_params["train"], seed=self.dropout_params["seed"])
-                dropout_cache[l] = mask
-                #print("shape of input to layer %d: "%l, input_next.shape)
-            else:
-                input_next = activation_out.copy()
-                #print("shape of input to layer %d: " % l, input_next.shape)
-            linear_cache[l] = linear_forward(input_next, self.params["W%d"%l], self.params["b%d"%l])
-            #print("shape of linear cache %d: " % l, linear_cache[l].shape)
-            activation_out = relu_forward(linear_cache[l])
-            relu_cache[l] = activation_out
-            #print("shape of relu cache %d: " % l, relu_cache[l].shape)
+                input_next, dropout_cache[l-1] = self.apply_forward_dropout(input_next)
+
+            linear_cache[l] = linear_forward(input_next, self.params["W%d" % l],
+                                             self.params["b%d" % l])
+            input_next = relu_forward(linear_cache[l])
+            relu_cache[l] = input_next.copy()
 
         # last hidden layer -> output layer
-        l += 1
         if self.use_dropout:
-            input_next, mask = dropout_forward(activation_out, p=self.dropout_params["p"],
-                train=self.dropout_params["train"], seed=self.dropout_params["seed"])
-            dropout_cache[l] = mask
-            #print("shape of dropout to layer %d: " % l, input_next.shape)
-        else:
-            input_next = activation_out.copy()
-            #print("shape of input to layer %d: " % l, input_next.shape)
-        linear_cache[l] = linear_forward(input_next, self.params["W%d"%l], self.params["b%d"%l])
-        # activation_out = relu_forward(linear_cache[l])
-        # relu_cache[l] = activation_out
-        #print("shape of linear cache %d: " % l, linear_cache[l].shape)
-        # activation_out = relu_forward(linear_cache[l])
-        # relu_cache[l] = activation_out
+            input_next, dropout_cache[self.num_layers-1] = self.apply_forward_dropout(input_next)
 
-        scores = linear_cache[l].copy() #activation_out.copy()
+        linear_cache[self.num_layers] = linear_forward(input_next,
+                                                       self.params["W%d" % self.num_layers],
+                                                       self.params["b%d" % self.num_layers])
+        scores = linear_cache[self.num_layers].copy()
         #######################################################################
         #                            END OF YOUR CODE                         #
         #######################################################################
@@ -205,30 +187,24 @@ class FullyConnectedNet(object):
         #######################################################################
         #                           BEGIN OF YOUR CODE                        #
         #######################################################################
-        # print(self.dropout_params)
         loss, dlogits = softmax(scores, y)
-        #print("shape of dlogits: ", dlogits.shape)
         # add L2 regularization
         loss += 0.5 * self.reg * np.sum([np.sum(self.params["W%d" % l]**2)
-                                          for l in range(1, self.num_layers + 1)])
+                                         for l in range(1, self.num_layers + 1)])
 
         # last hidden layer <- output layer
         dX_, dW_, db_ = linear_backward(dlogits,
-                                     relu_cache[self.num_layers-1],
-                                     self.params["W%d"%(self.num_layers)],
-                                     self.params["b%d" %(self.num_layers)])
+                                        relu_cache[self.num_layers-1],
+                                        self.params["W%d" % self.num_layers],
+                                        self.params["b%d" % self.num_layers])
         # add regularization effect to W
-        grads["W%d"%(self.num_layers)] = dW_ + self.reg * self.params["W%d"%(self.num_layers)]
-        grads["b%d" % (self.num_layers)] = db_
-        #dX_ = relu_backward(dX_, )
+        grads["W%d" % self.num_layers] = dW_ + self.reg * self.params["W%d" % self.num_layers]
+        grads["b%d" % self.num_layers] = db_.copy()
+
         # hidden layer i <- hidden layer i+1
         for l in reversed(range(2, self.num_layers)):
-            #print(l)
-            # dX_ = dropout_backward(dX_, p=self.dropout_params["p"],
-            #         train=self.dropout_params["train"], seed=self.dropout_params["seed"])
             if self.use_dropout:
-                dX_ = dropout_backward(dX_, dropout_cache[l],
-                        p=self.dropout_params["p"], train=self.dropout_params["train"])
+                dX_ = self.apply_backward_dropout(dX_, dropout_cache[l])
             dX_ = relu_backward(dX_, linear_cache[l])
             dX_, dW_, db_ = linear_backward(dX_,
                                             relu_cache[l-1],
@@ -239,8 +215,7 @@ class FullyConnectedNet(object):
             grads["b%d" % l] = db_
         # input layer <- first hidden layer
         if self.use_dropout:
-            dX_ = dropout_backward(dX_, dropout_cache[1],
-                    p=self.dropout_params["p"], train=self.dropout_params["train"])
+            dX_ = self.apply_backward_dropout(dX_, dropout_cache[1])
         dX_ = relu_backward(dX_, linear_cache[1])
         dX_, dW_, db_ = linear_backward(dX_,
                                         X,
@@ -249,10 +224,6 @@ class FullyConnectedNet(object):
         # add regularization effect to W
         grads["W1"] = dW_ + self.reg * self.params["W1"]
         grads["b1"] = db_
-
-
-
-
         #######################################################################
         #                            END OF YOUR CODE                         #
         #######################################################################
